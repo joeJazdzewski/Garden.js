04d898bf5414e8141519272ea39b1d53d51807e2
Joseph Jazdzewski <thesantacruzian@gmail.com>
2026-01-10 13:57:22 -0800

    fix the test runner

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,7 @@ export default {
   extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['<rootDir>/src/test/**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
